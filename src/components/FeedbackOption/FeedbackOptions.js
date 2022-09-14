d693bbdfc72d3eeb53398a3983456ff3789ebda7
Evgenii Horokhov <gorohovevgeniyvalerievich@gmail.com>
2022-09-15 00:41:30 +0300

    1task

---
+++ src/components/FeedbackOption/FeedbackOptions.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import shortid from 'shortid';
+
+export default function FeedbackOptions({ options, onLeaveFeedback }) {
+    return (
+		<>
+			{options.map((option) => (
+				<button key={shortid.generate()} type="button" name={option} onClick={onLeaveFeedback}>
+					{option}
+				</button>
+			))}
+		</>
+	);
+};
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
